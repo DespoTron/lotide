3431daf3b6c5be6b65cd5b042ac0a224b0a4b278
Thai Do <thai.hl.do@gmail.com>
2020-08-17 23:34:34 +0000

    setting up my own assertEquals function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const mad = String.fromCodePoint(0x1F621);
+const flame = String.fromCodePoint(0x1F525);
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log(flame + flame + flame + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(mad + mad + mad + "Assertion Failed: " + "\"" + actual + "\"" + " !== " + "\"" + expected + "\"");
+  }
+};
+
+assertEquals("Lighthouse labs", "Bootcamp");
+assertEquals(1, 1);
+assertEquals("test", "test");
+assertEquals(1, 5);
