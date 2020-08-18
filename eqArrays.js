172218561fab6d0eed1083f7f26c83aa2aa12ebf
Thai Do <thai.hl.do@gmail.com>
2020-08-18 14:39:31 +0000

    first commit for eqArrays.js to compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const mad = String.fromCodePoint(0x1F621);
+const flame = String.fromCodePoint(0x1F525);
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${flame}${flame} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${mad}${mad} Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const eqArrays = function(myArr, myArrTwo) {
+  for(let i = 0; i < myArr.length; i++) {
+    if(myArr[i] !== myArrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let a = [1,2,3];
+let b = [1,2,3];
+let c = [3,4,5];
+let d = ["1", "2", "3"]
+let e = ["1", "2", "3"]
+let f = ["1", "2", 3];
+
+console.log(eqArrays(d, e));
+console.log(eqArrays(e, f));
+console.log(assertEquals(eqArrays(a,b), true));
