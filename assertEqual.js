17fd19f09ce2f6f5c68c8d22024680875b8a530f
Thai Do <thai.hl.do@gmail.com>
2020-08-18 01:50:10 +0000

    refractored with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const mad = String.fromCodePoint(0x1F621);
 const flame = String.fromCodePoint(0x1F525);
 const assertEquals = function(actual, expected) {
   if (actual === expected) {
-    console.log(flame + flame + flame + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${flame}${flame}${flame} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(mad + mad + mad + "Assertion Failed: " + "\"" + actual + "\"" + " !== " + "\"" + expected + "\"");
+    console.log(`${mad}${mad}${mad} Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
 
