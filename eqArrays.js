6f3f447473f7ae661bb1d8fe50fb1e7ca36a7c28
Thai Do <thai.hl.do@gmail.com>
2020-08-23 05:27:49 +0000

    refactored so file contains only the function with a separate test file in another folder

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const mad = String.fromCodePoint(0x1F621);
-const flame = String.fromCodePoint(0x1F525);
-const assertEquals = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${flame}${flame} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${mad}${mad} Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
 const eqArrays = function(myArr, myArrTwo) {
   if(myArr.length !== myArrTwo.length) return false;
 
@@ -20,12 +9,5 @@ const eqArrays = function(myArr, myArrTwo) {
   return true;
 };
 
-let a = [1, 2, 3];
-let b = [1, 2, 3];
-let d = ["1", "2", "3"];
-let e = ["1", "2", "3"];
-let f = ["1", "2", 3];
+module.exports = eqArrays;
 
-console.log(eqArrays(d, e));
-console.log(eqArrays(e, f));
-console.log(assertEquals(eqArrays(a, b), true));
