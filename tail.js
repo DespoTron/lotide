ea6416a7381bfda4897f1783d3f11e67c28a8fe3
Thai Do <thai.hl.do@gmail.com>
2020-08-23 05:25:33 +0000

    refactored tail to export it and created a separate test file

--- tail.js
+++ tail.js
@@ -1,28 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const mad = String.fromCodePoint(0x1F621);
-const flame = String.fromCodePoint(0x1F525);
-const assertEquals = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${flame}${flame}${flame} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${mad}${mad}${mad} Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-
 const tail = function(myArr) {
   let newArr = [];
   newArr = myArr.slice(1);
   return newArr;
 };
 
-const a = [1,2,3];
-console.log(tail(a));
-console.log(a);
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEquals(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEquals(result, ["Lighthouse", "Labs"]); // => will always fail!
