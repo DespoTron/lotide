79c8d9678cba3f7a4de04f81824f9f2d210ba8da
Thai Do <thai.hl.do@gmail.com>
2020-08-23 11:25:15 +0000

    Refactored to show only function

--- flatten.js
+++ flatten.js
@@ -1,29 +1,9 @@
-const eqArrays = function(myArr, myArrTwo) {
-  for (let i = 0; i < myArr.length; i++) {
-    if (myArr[i] !== myArrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-// function that was created for this assignment
-const assertArraysEqual = function(firstArr, secondArr) {
-  const mad = String.fromCodePoint(0x1F621);
-  const flame = String.fromCodePoint(0x1F525);
-  if(eqArrays(firstArr, secondArr)) {
-    console.log(`${flame} Assertion Passed: ${firstArr} === ${secondArr}`)
-  } else {
-    console.log(`${mad}$ Assertion Failed: ${firstArr} !== ${secondArr}`);
-  }
-}
-
 const flatten = function(newArr) {
   let output = [];
   for(let i = 0; i < newArr.length; i++) {
     if(Array.isArray(newArr[i])) {
       for(let j = 0; j < newArr[i].length; j++) {
         output.push(newArr[i][j]);
-        console.log(output)
       }
     } else {
       output.push(newArr[i])
@@ -32,5 +12,5 @@ const flatten = function(newArr) {
   return output
 };
 
-const a = [1,2, [3,4], 5, [6]];
-console.log(flatten(a));
+module.exports = flatten
+
