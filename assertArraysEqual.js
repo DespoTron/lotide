945fa09cd888c418dbeea6508bb875118a25d1dc
Thai Do <thai.hl.do@gmail.com>
2020-08-23 05:28:34 +0000

    refactored to contain only the function with a separate test case file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,5 @@
-const eqArrays = function(myArr, myArrTwo) {
-  for (let i = 0; i < myArr.length; i++) {
-    if (myArr[i] !== myArrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
+
 // function that was created for this assignment
 const assertArraysEqual = function(firstArr, secondArr) {
   const mad = String.fromCodePoint(0x1F621);
@@ -13,15 +7,9 @@ const assertArraysEqual = function(firstArr, secondArr) {
   if(eqArrays(firstArr, secondArr)) {
     console.log(`${flame} Assertion Passed: ${firstArr} === ${secondArr}`)
   } else {
-    console.log(`${mad}$ Assertion Failed: ${firstArr} !== ${secondArr}`);
+    console.log(`${mad} Assertion Failed: ${firstArr} !== ${secondArr}`);
   }
 }
 
-// -------------- TESTING FUNCTION ------------
+module.exports = assertArraysEqual;
 
-//const a = [1,2,3];
-//const b = [1,2,3];
-//const c = [4,5,6];
-//const d = [7,8,9];
-//console.log(assertArraysEqual(a, b));
-//console.log(assertArraysEqual(c, d));
