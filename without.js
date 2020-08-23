fbc75dad610bae195b1eb11e28ed7ed20ee60df7
Thai Do <thai.hl.do@gmail.com>
2020-08-23 11:26:14 +0000

    Refactored function

--- without.js
+++ without.js
@@ -28,11 +28,5 @@ const without = function(source, itemsToRemove) {
   return newArr;
 };
 
-// ---------------- TEST CASES ---------------
+module.exports = without;
 
-//console.log(without([1, 2, 3], [1])); // => [2, 3]
-//console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]]
-//const words = ["hello", "world", "lighthouse"];
-//console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-//console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
